e20cd489d87d472edc85e100eba891045cfbc9d9
Carlos Sosa <carlosneftaly@hotmail.com>
2022-04-12 12:30:52 -0500

    Exercise for lesson 72 done

---
+++ Dtos/MovieDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public Genre Genre { get; set; }
+        [Required]
+        public byte GenreId { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        [Range(1, 20, ErrorMessage = "It should be between 1 and 20")]
+        public int NumberInStock { get; set; }
+    }
+}
